09c4ef87b731c585452ec9814dd210c3f2778c71
DannyRuleZ <101912775+DannyRuleZ@users.noreply.github.com>
2025-03-21 03:27:46 -0400

    Update OdaInv.py

--- OdaInv.py
+++ OdaInv.py
@@ -12,20 +12,20 @@ with   col1:
     st.title("")
     st.title("Buenos dias Oda!!")
 with col2:
-    st.image("invitation/BuenosDias.jpg")
+    st.image("Invitation/BuenosDias.jpg")
 
 col1, col2, col3 = st.columns(3)
 with col1:
     st.subheader("")
 with col2:
-    st.image("invitation/next.png")
+    st.image("Invitation/next.png")
 with col3:
     st.subheader("")
 
 
 col1, col2 = st.columns(2)
 with   col1:
-    st.image("invitation/YaValimos.jpg")
+    st.image("Invitation/YaValimos.jpg")
 with col2:
     st.title("")
     st.title("Sobre la apuesta de ayer..")
